e2102a22a3607454568f966eff8aa14c3aebe295
Qu4s4r <98132870+Qu4s4rM@users.noreply.github.com>
2024-10-19 20:11:15 -0500

    Update launch.sh

--- rofi/launcher/launch.sh
+++ rofi/launcher/launch.sh
@@ -10,10 +10,22 @@
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
 
+FILE="$HOME/.cache/rofi-launcher.lock"
 dir="$HOME/.config/rofi/launcher/"
 theme='colors'
 
 ## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+#rofi \
+#    -show drun \
+#    -theme ${dir}/${theme}.rasi
+
+if [[ ! -f "$FILE" ]]; then
+    touch "$FILE"
+    rofi -show drun -theme ${dir}/${theme}.rasi
+    sleep 10
+    killall rofi
+elif [[ -f "$FILE" ]]; then
+    killall rofi
+    rm -rf "$FILE"
+fi
+
