8cd73bdf24f9b29bc309b5cef5c708ca520e946a
Qu4s4r <98132870+Qu4s4rM@users.noreply.github.com>
2024-11-09 09:56:55 -0500

    Update configVol.js

--- ags/panels/configVol.js
+++ ags/panels/configVol.js
@@ -3,7 +3,6 @@ const notifications = await Service.import("notifications")
 const mpris = await Service.import("mpris")
 const audio = await Service.import("audio")
 const battery = await Service.import("battery")
-const systemtray = await Service.import("systemtray")
 
 const WINDOW_NAME = "osd"
 
