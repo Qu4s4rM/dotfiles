7fff3d661684984dc43238b80009f2ae69ff564c
Qu4s4r <98132870+Qu4s4rM@users.noreply.github.com>
2024-11-09 09:56:36 -0500

    Update config.js

--- ags/config.js
+++ ags/config.js
@@ -3,7 +3,6 @@ const notifications = await Service.import("notifications")
 const mpris = await Service.import("mpris")
 const audio = await Service.import("audio")
 const battery = await Service.import("battery")
-const systemtray = await Service.import("systemtray")
 
 const date = Variable("", {
     poll: [1000, 'date "+%H:%M"'],
